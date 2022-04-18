692d899d48e30bdff16ea8226340c97b384861f0
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-04-18 18:11:07 +0900

    프로젝트 생성 및 환경 설정

---
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/VolunteerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.volunteer_platform.volunteer_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerPlatformApplication.class, args);
+	}
+
+}
---
+++ volunteer_platform/src/test/java/com/volunteer_platform/volunteer_platform/VolunteerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteer_platform.volunteer_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
