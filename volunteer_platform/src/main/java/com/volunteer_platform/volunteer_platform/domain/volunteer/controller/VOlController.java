6c2b2c3229e812aa320b68a6f352ffffac85eff3
han-gyeong <hangyeong@outlook.com>
2022-05-16 21:51:41 +0900

    edit: VolController 구현체 의존에서 인터페이스 의존으로 수정

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/VOlController.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/VOlController.java
@@ -3,9 +3,9 @@ package com.volunteer_platform.volunteer_platform.domain.volunteer.controller;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.controller.form.Form;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.VolActivity;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.VolOrgan;
-import com.volunteer_platform.volunteer_platform.domain.volunteer.service.VolActivityServiceImpl;
-import com.volunteer_platform.volunteer_platform.domain.volunteer.service.VolActivityTimeServiceImpl;
-import com.volunteer_platform.volunteer_platform.domain.volunteer.service.VolOrganServiceImpl;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.service.volinterface.VolActivityService;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.service.volinterface.VolActivityTimeService;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.service.volinterface.VolOrganService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,17 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class VOlController {
 
-    private final VolActivityServiceImpl volActivityService;
-    private final VolActivityTimeServiceImpl volActivityTimeService;
-    private final VolOrganServiceImpl volOrganService;
+    private final VolActivityService volActivityService;
+    private final VolActivityTimeService volActivityTimeService;
+    private final VolOrganService volOrganService;
 
     @PostMapping("/create/vol")
     public String createVol(@RequestBody Form form) {
 
         VolOrgan volOrgan = volOrganService.createVolOrgan(form);
-
         VolActivity volActivity = volActivityService.createVolActivity(form, volOrgan);
-
         volActivityTimeService.createVolActivityTime(form, volActivity);
 
         return "redirect:/";
