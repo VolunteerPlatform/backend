7e558b91acd091a708a6409aab1319956426d0ed
han-gyeong <hangyeong@outlook.com>
2022-05-30 12:06:56 +0900

    edit: 활동 완료 표시를 위한 Enum 타입 추가

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/models/enumtype/IsAuthorized.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/models/enumtype/IsAuthorized.java
@@ -1,5 +1,5 @@
 package com.volunteer_platform.volunteer_platform.domain.volunteer.models.enumtype;
 
 public enum IsAuthorized {
-    APPROVAL, WAITING, DISAPPROVAL
+    APPROVAL, WAITING, DISAPPROVAL, COMPLETE
 }
