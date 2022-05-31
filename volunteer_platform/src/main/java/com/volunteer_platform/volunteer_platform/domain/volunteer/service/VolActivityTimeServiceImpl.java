afa73fa5f3ff8c8b88ecc46e50eb7d11761498e1
han-gyeong <hangyeong@outlook.com>
2022-05-31 20:46:57 +0900

    edit: VolActivityTime 활동 기간동안 날짜 단위로 생성하도록 변경

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/service/VolActivityTimeServiceImpl.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/service/VolActivityTimeServiceImpl.java
@@ -3,6 +3,7 @@ package com.volunteer_platform.volunteer_platform.domain.volunteer.service;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.controller.form.VolActivityTimeForm;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.VolActivity;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.VolActivityTime;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.models.enumtype.ActivityTimeStatus;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.repository.VolActivityTimeRepository;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.service.volinterface.VolActivityTimeService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -30,15 +34,38 @@ public class VolActivityTimeServiceImpl implements VolActivityTimeService {
     @Override
     @Transactional
     public void createVolActivityTime(List<VolActivityTimeForm> volActivityTimeForms, VolActivity volActivity) {
+        HashMap<DayOfWeek, VolActivityTimeForm> activityTimeMap = new HashMap<>();
+
         volActivityTimeForms.forEach(form -> {
-            VolActivityTime volActivityTime = VolActivityTime.builder()
+            if (activityTimeMap.get(form.getActivityWeek()) != null) {
+                throw new IllegalArgumentException("요일 당 하나의 시간만 추가 가능합니다.");
+            }
+
+            activityTimeMap.put(form.getActivityWeek(), form);
+        });
+
+        for (LocalDate date = volActivity.getActivityPeriod().getBegin();
+             date.isBefore(volActivity.getActivityPeriod().getEnd().plusDays(1));
+             date = date.plusDays(1)) {
+
+            VolActivityTimeForm timeForm = activityTimeMap.get(date.getDayOfWeek());
+
+            if (timeForm == null) {
+                continue;
+            }
+
+            VolActivityTime activityTime = VolActivityTime.builder()
                     .volActivity(volActivity)
-                    .activityWeek(form.getActivityWeek())
-                    .startTime(form.getStartTime())
-                    .endTime(form.getEndTime())
+                    .activityWeek(date.getDayOfWeek())
+                    .startTime(timeForm.getStartTime())
+                    .endTime(timeForm.getEndTime())
+                    .activityDate(date)
+                    .numOfRecruit(volActivity.getNumOfRecruit())
+                    .numOfApplicant(0)
+                    .timeStatus(ActivityTimeStatus.RECRUITING)
                     .build();
 
-            saveVolActivityTime(volActivityTime);
-        });
+            saveVolActivityTime(activityTime);
+        }
     }
 }
