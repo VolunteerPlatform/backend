f7cc2818d0e1f70bbd4ff4614dda93ae8e9f0fdf
han-gyeong <hangyeong@outlook.com>
2022-06-29 16:29:16 +0900

    edit: 시작일, 종료일 검증을 Period 생성시 하도록 변경

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/models/Period.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/models/Period.java
@@ -1,6 +1,8 @@
 package com.volunteer_platform.volunteer_platform.domain.volunteer.models;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.time.LocalDate;
@@ -8,10 +10,16 @@ import java.time.LocalDate;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
 public class Period {
-
     private LocalDate begin;
     private LocalDate end;
+
+    public Period(String begin, String end) {
+        this.begin = LocalDate.parse(begin);
+        this.end = LocalDate.parse(end);
+
+        if (this.begin.compareTo(this.end) > 0) {
+            throw new IllegalArgumentException("시작일은 종료일보다 이후일 수 없습니다.");
+        }
+    }
 }
