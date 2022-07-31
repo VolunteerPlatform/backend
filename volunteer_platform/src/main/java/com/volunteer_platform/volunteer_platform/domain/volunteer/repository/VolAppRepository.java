94884fbdfe2cb369288241f25cd634b3013654de
이종수 <81428357+Lee-JongSoo@users.noreply.github.com>
2022-07-31 10:43:18 +0900

    Feat: 상태코드 관리 class

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/repository/VolAppRepository.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/repository/VolAppRepository.java
@@ -11,9 +11,18 @@ import java.util.List;
 
 public interface VolAppRepository extends JpaRepository<AppHistory, Long> {
 
-    @Query("select m from AppHistory m join fetch m.volActivityTime v join fetch v.volActivity vo join fetch vo.volOrgan where m.member.id = ?1")
+    @Query("select m from AppHistory m " +
+            "join fetch m.volActivityTime v " +
+            "join fetch v.volActivity vo " +
+            "join fetch vo.volOrgan " +
+            "where m.member.id = ?1")
     List<AppHistory> findByMemberId(Long memberId);
 
-    @Query("select m from AppHistory m join fetch m.volActivityTime v join fetch m.member mm join fetch mm.memberInfo join fetch mm.member1365Info where v.volActivity.id = ?1 and m.isAuthorized = ?2 and v.activityDate = ?3")
+    @Query("select m from AppHistory m " +
+            "join fetch m.volActivityTime v " +
+            "join fetch m.member mm " +
+            "join fetch mm.memberInfo " +
+            "join fetch mm.member1365Info " +
+            "where v.volActivity.id = ?1 and m.isAuthorized = ?2 and v.activityDate = ?3")
     List<AppHistory> findApplicantsByCondition(Long activityId, IsAuthorized isAuthorized, LocalDate activityDate, Pageable pageable);
 }
