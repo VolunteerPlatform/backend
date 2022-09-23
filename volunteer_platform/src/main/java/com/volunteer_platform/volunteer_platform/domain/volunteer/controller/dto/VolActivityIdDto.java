6ec4290fe2f5c50062c5dcc2317389f52f2f8cf2
han-gyeong <hangyeong@outlook.com>
2022-09-23 16:40:28 +0900

    Edit: AppHistoryDto에 memberId 추가, ActivityIdDto에 인자없는 생성자 추가

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/dto/VolActivityIdDto.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/dto/VolActivityIdDto.java
@@ -1,8 +1,10 @@
 package com.volunteer_platform.volunteer_platform.domain.volunteer.controller.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class VolActivityIdDto {
 
     private Long activityId;
