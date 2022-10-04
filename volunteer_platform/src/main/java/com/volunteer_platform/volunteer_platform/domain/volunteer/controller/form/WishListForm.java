ea87df1a1c7ec1d17a3ff92b5c5ef5d1d16e4b21
han-gyeong <hangyeong@outlook.com>
2022-10-04 22:04:31 +0900

    Feat: 회원별 찜한 봉사 목록 확인 기능 추가, 찜하기 예외처리 추가 및 반환값 수정

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/form/WishListForm.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/form/WishListForm.java
@@ -1,10 +1,14 @@
 package com.volunteer_platform.volunteer_platform.domain.volunteer.controller.form;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WishListForm {
     private Long sessionId;
 }
--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/service/WishListServiceImpl.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/service/WishListServiceImpl.java
@@ -1,41 +1,58 @@
 package com.volunteer_platform.volunteer_platform.domain.volunteer.service;
 
-import com.volunteer_platform.volunteer_platform.domain.volunteer.converter.CustomResponse;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.controller.dto.SearchResultDto;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.WishList;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.repository.VolActivitySessionRepository;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.repository.WishListRepository;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.service.volinterface.WishListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
-
-import static com.volunteer_platform.volunteer_platform.domain.volunteer.converter.CustomResponse.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WishListServiceImpl implements WishListService {
 
     private final WishListRepository wishListRepository;
+    private final VolActivitySessionRepository volActivitySessionRepository;
 
     @Override
     @Transactional
-    public DTOResponse addWishList(Long memberId, Long sessionId) {
+    public void addWishList(Long memberId, Long sessionId) {
+        isExistSession(sessionId);
 
         Optional<WishList> wishList = wishListRepository.findWishList(memberId, sessionId);
+        if (wishList.isPresent()) {
+            wishListRepository.deleteById(wishList.get().getId());
+        } else {
+            WishList newWishList = WishList.builder()
+                    .memberId(memberId)
+                    .sessionId(sessionId)
+                    .build();
 
-        wishList.ifPresent(wishListRepository::delete);
+            wishListRepository.save(newWishList);
+        }
+    }
 
-        WishList newWishList = WishList.builder()
-                .memberId(memberId)
-                .sessionId(sessionId)
-                .build();
+    @Override
+    public List<SearchResultDto> getWishList(Long memberId) {
+        List<Long> wishedSession = wishListRepository.findByMemberId(memberId).stream()
+                .map(o -> o.getSessionId())
+                .collect(Collectors.toList());
 
-        wishListRepository.save(newWishList);
+        return volActivitySessionRepository.findAllById(wishedSession)
+                .stream()
+                .map(SearchResultDto::of)
+                .collect(Collectors.toList());
+    }
 
-        String message = "성공적으로 찜하기되었습니다.";
-
-        return new DTOResponse(HttpStatus.CREATED.value(), message, message);
+    private void isExistSession(Long sessionId) {
+        if (!volActivitySessionRepository.existsById(sessionId)) {
+            throw new IllegalArgumentException("존재하지 않는 세션 ID 입니다.");
+        }
     }
 }
