aaee22c376e46b92fda4d274abdb4b85e96eea58
han-gyeong <hangyeong@outlook.com>
2022-10-18 19:03:04 +0900

    Fix: 뷰에 맞게 지원내역에 카테고리와 활동방법 추가

--- volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/dto/AppHistoryDto.java
+++ volunteer_platform/src/main/java/com/volunteer_platform/volunteer_platform/domain/volunteer/controller/dto/AppHistoryDto.java
@@ -1,6 +1,8 @@
 package com.volunteer_platform.volunteer_platform.domain.volunteer.controller.dto;
 
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.AppHistory;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.models.enumtype.ActivityMethod;
+import com.volunteer_platform.volunteer_platform.domain.volunteer.models.enumtype.Category;
 import com.volunteer_platform.volunteer_platform.domain.volunteer.models.enumtype.IsAuthorized;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,6 +29,10 @@ public class AppHistoryDto {
 
     private String organization;
 
+    private Category category;
+
+    private ActivityMethod activityMethod;
+
     private Long memberId;
 
     public static AppHistoryDto of(AppHistory appHistory) {
@@ -41,6 +47,8 @@ public class AppHistoryDto {
                 .activitySummary(appHistory.getVolActivitySession().getVolActivity().getActivitySummary())
                 .organization(appHistory.getVolActivitySession().getVolActivity().getVolOrgan().getName())
                 .memberId(appHistory.getMember().getId())
+                .category(appHistory.getVolActivitySession().getVolActivity().getCategory())
+                .activityMethod(appHistory.getVolActivitySession().getVolActivity().getActivityMethod())
                 .build();
     }
 }
